ae5ed1ddc2b5f2385dc406a6b80050cb6933c625
vicnete3456 <165092934+vicnete3456@users.noreply.github.com>
2024-04-16 19:02:02 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+@bot.command(name='list')
+async def _list(ctx, arg):
+    pass
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+
+bot.run("token")
